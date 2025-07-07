e64c5a1fa830a5f82dec2da291aab7a5524e8803
Rahul Shenoy <rahulshenoykarkala@gmail.com>
2025-07-07 14:56:04 +0530

    Added Type definition for Character

---
+++ src/types/index.ts
@@ -0,0 +1,17 @@
+interface Planet {
+    id: String;
+    name: String;
+}
+
+export interface Character {
+    id: String,
+    name: String,
+    gender: String,
+    skin_color: String,
+    hair_color: String,
+    height: Number,
+    eye_color: String,
+    mass: Number,
+    url: String,
+    birth_year: String;
+}
