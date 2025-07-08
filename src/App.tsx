e5b6c821d6ae1952c63b412f883816a10aea69ec
Rahul Shenoy <rahulshenoykarkala@gmail.com>
2025-07-08 21:17:30 +0530

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import './App.css'
 import CharacterList from './Components/templates/CharacterList';
-import { character } from './mock';
 import { store } from './store';
 import { Provider } from 'react-redux';
+import Header from './Components/molecules/header';
 
 function App() {
   return (
     <Provider store={store}>
-      <div>
-        <CharacterList list={[]}/>
+      <div style={{ width: '100vw', height: '100vh' }}>
+        <Header />
+        <CharacterList />
       </div>
     </Provider>
   )
